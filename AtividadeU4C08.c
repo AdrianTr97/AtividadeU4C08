652773f4422d808e5310cc03f77e5cf4aea40e3b
Adrian Trajano <45612732+AdrianTr97@users.noreply.github.com>
2025-02-19 16:41:09 -0300

    correção no quadrado do display.c

--- AtividadeU4C08.c
+++ AtividadeU4C08.c
@@ -7,12 +7,7 @@
 #include "lib/ssd1306.h"
 #include "lib/font.h"
 
-//#define I2C_PORT i2c0
-//#define I2C_SDA 8
-//#define I2C_SCL 9
-//#define VRX_PIN 26   
-//#define VRY_PIN 27   
-//#define SW_PIN 22
+
 #define I2C_PORT i2c1
 #define I2C_SDA 14
 #define I2C_SCL 15
@@ -42,11 +37,21 @@ uint pwm_init_gpio(uint gpio, uint wrap) {
     return slice_num;  // Retorna o número do slice para uso posterior
 }
 
+// Mapeamento do ADC (0-4095) para a resolução do display (0-120 para X e 0-56 para Y)
+//int map_value(int value, int from_low, int from_high, int to_low, int to_high) {
+//  return to_low + ((value - from_low) * (to_high - to_low)) / (from_high - from_low);
+//}
+
+// Mapeamento do ADC (0-4095) para a resolução do display (0-120 para X e 0-56 para Y)
+int map_value(int value, int from_low, int from_high, int to_low, int to_high) {
+  return to_low + ((value - from_low) * (to_high - to_low)) / (from_high - from_low);
+}
+
 void gpio_config(){
-    // Inicializa o pino do LED verde
+    // Inicializa o pino do LED azul
     gpio_init(LEDB_PIN);            // Inicializa o pino do LED verde
     gpio_set_dir(LEDB_PIN, GPIO_OUT);  // Configura o pino como saída
-    gpio_put(LEDB_PIN, false);      // Inicializa o LED verde apagado (false)
+    gpio_put(LEDB_PIN, false);      // Inicializa o LED azul apagado (false)
 }
 
 int main(){
@@ -55,14 +60,13 @@ int main(){
     stdio_init_all();
     
     gpio_config();
+    // Variáveis de controle
     bool led_estado = false;  // Flag para controlar o estado do LED (false = desligado, true = ligado), para o led verde
+    bool pwm_enabled = true;  // Controle do PWM (se habilitado ou desabilitado)
 
-    // Inicializa o PWM para o LED no pino GP12
-    uint pwm_wrap = 4096;  // Define o valor máximo para o contador do PWM (influencia o duty cycle)
-    uint pwm_slice = pwm_init_gpio(LEDG_PIN, pwm_wrap);  // Inicializa o PWM no pino LED_PIN e retorna o número do slice, green eixo y, para cima
-    uint pwm_slice = pwm_init_gpio(LEDR_PIN, pwm_wrap);  // Inicializa o PWM no pino LED_PIN e retorna o número do slice, red eixo x, para esquerda
-
-    uint32_t last_print_time = 0; // Variável para armazenar o tempo da última impressão na serial
+    // Posição inicial do quadrado
+    int y = 28;
+    int x = 60;
     
     gpio_init(JOYSTICK_PB);
     gpio_set_dir(JOYSTICK_PB, GPIO_IN);
@@ -97,7 +101,15 @@ int main(){
     //char str_x[5];  // Buffer para armazenar a string
     //char str_y[5];  // Buffer para armazenar a string  
     
-    bool cor = true;
+    bool cor = true; // Cor do quadrado (pode ser ajustada conforme necessário)
+
+    // Inicializa o PWM para o LED no pino GP12
+    uint pwm_wrap = 4096;  // Define o valor máximo para o contador do PWM (influencia o duty cycle)
+    uint pwm_slice_x = pwm_init_gpio(LEDR_PIN, pwm_wrap);  // Inicializa o PWM no pino LEDR_PIN e retorna o número do slice, red eixo x, para esquerda
+    uint pwm_slice_y = pwm_init_gpio(LEDG_PIN, pwm_wrap);  // Inicializa o PWM no pino LEDG_PIN e retorna o número do slice, green eixo y, para cima
+    
+    uint32_t last_print_time = 0; // Variável para armazenar o tempo da última impressão na serial
+
     while (true)
     {
       adc_select_input(0); // Seleciona o ADC para eixo X. O pino 26 (VRX) como entrada analógica
@@ -105,32 +117,35 @@ int main(){
       uint16_t vrx_value = adc_read();  // Lê o valor analógico do eixo X, retornando um valor entre 0 e 4095
       adc_select_input(1); // Seleciona o ADC para eixo Y. O pino 27 como entrada analógica
       adc_value_y = adc_read();    
-      uint16_t vry_value = adc_read();  // Lê o valor analógico do eixo X, retornando um valor entre 0 e 4095
+      uint16_t vry_value = adc_read();  // Lê o valor analógico do eixo Y, retornando um valor entre 0 e 4095
       //sprintf(str_x, "%d", adc_value_x);  // Converte o inteiro em string
       //sprintf(str_y, "%d", adc_value_y);  // Converte o inteiro em string
 
-      // Controle do LED com PWM baseado no valor do ADC0 (VRX)
+      // Controle dos LEDs com PWM baseado no valor do ADC0 (VRX) e ADC1 (VRY)
       pwm_set_gpio_level(LEDR_PIN, vrx_value);  // Ajusta o duty cycle do LED proporcional ao valor lido de VRX
+      pwm_set_gpio_level(LEDG_PIN, vry_value);  // Ajusta o duty cycle do LED proporcional ao valor lido de VRY
 
       // Calcula o duty cycle em porcentagem para impressão
-      float duty_cycle = (vrx_value / 4095.0) * 100;  // Converte o valor lido do ADC em uma porcentagem do duty cycle
+      float duty_cycle_x = (vrx_value / 4095.0) * 100;  // Converte o valor lido do ADC em uma porcentagem do duty cycle
+      float duty_cycle_y = (vry_value / 4095.0) * 100;  // Converte o valor lido do ADC em uma porcentagem do duty cycle, green
 
       // Imprime os valores lidos e o duty cycle proporcional na comunicação serial a cada 1 segundo
       uint32_t current_time = to_ms_since_boot(get_absolute_time());  // Obtém o tempo atual desde o boot do sistema
       if (current_time - last_print_time >= 1000) {  // Verifica se passou 1 segundo desde a última impressão
           printf("VRX: %u\n", vrx_value);  // Imprime o valor lido de VRX no console serial
-          printf("Duty Cycle LED: %.2f%%\n", duty_cycle);  // Imprime o duty cycle calculado em porcentagem
+          printf("Duty Cycle LED_R: %.2f%%\n", duty_cycle_x);  // Imprime o duty cycle calculado em porcentagem
+          printf("VRY: %u\n", vry_value);  // Imprime o valor lido de VRX no console serial
+          printf("Duty Cycle LED_G: %.2f%%\n", duty_cycle_y);  // Imprime o duty cycle calculado em porcentagem, green
           last_print_time = current_time;  // Atualiza o tempo da última impressão
       }
-
-      // Introduz um atraso de 100 milissegundos antes de repetir a leitura
-      sleep_ms(100);  // Pausa o programa por 100ms para evitar leituras e impressões muito rápidas
-
-
+      // Mapeia os valores do ADC para a posição do display
+      x = map_value(adc_value_y, 0, 4095, 0, 120); // 128 - 8 para manter dentro da tela
+      //y = map_value(adc_value_y, 0, 4095, 0, 56);  // 64 - 8 para manter dentro da tela
+      y = map_value(adc_value_x, 0, 4095, 56, 0);
       //cor = !cor;
       // Atualiza o conteúdo do display com animações
-      ssd1306_fill(&ssd, !cor); // Limpa o display
-      ssd1306_rect(&ssd, 1, 1, 126, 64, cor, !cor); // Desenha o retângulo externo 
+      ssd1306_fill(&ssd, !cor); // Limpa o display (128x64)
+      ssd1306_rect(&ssd, 1, 1, 126, 63, cor, !cor); // Desenha o retângulo externo 
       //ssd1306_line(&ssd, 3, 25, 123, 25, cor); // Desenha uma linha
       //ssd1306_line(&ssd, 3, 37, 123, 37, cor); // Desenha uma linha   
       //ssd1306_draw_string(&ssd, "CEPEDI   TIC37", 8, 6); // Desenha uma string
@@ -141,7 +156,7 @@ int main(){
       //ssd1306_draw_string(&ssd, str_x, 8, 52); // Desenha uma string     
       //ssd1306_line(&ssd, 84, 37, 84, 60, cor); // Desenha uma linha vertical      
       //ssd1306_draw_string(&ssd, str_y, 49, 52); // Desenha uma string   
-      //ssd1306_rect(&ssd, 52, 90, 8, 8, cor, !gpio_get(JOYSTICK_PB)); // Desenha um retângulo, preenche o retangulo na verdade 
+      //ssd1306_rect(&ssd, 52, 90, 8, 8, cor, !gpio_get(JOYSTICK_PB)); // Desenha um retângulo, preenche o retangulo ao clicar no analogico
       if (gpio_get(JOYSTICK_PB) == 0) {
         // Desenha apenas o contorno do novo retângulo quando o joystick for pressionado
         ssd1306_rect(&ssd, 3, 3, 122, 60, cor, !cor); 
@@ -151,11 +166,23 @@ int main(){
         // Atualiza o estado do LED verde com base no flag
         gpio_put(LEDB_PIN, led_estado); // Liga ou desliga o LED azul conforme o estado 
       }
-      ssd1306_rect(&ssd, 52, 102, 8, 8, cor, !gpio_get(Botao_A)); // Desenha um retângulo    
-      //ssd1306_rect(&ssd, 52, 114, 8, 8, cor, !cor); // Desenha o 3 retângulo aberto que nao serve para nada      
+      //ssd1306_rect(&ssd, 52, 102, 8, 8, cor, !gpio_get(Botao_A)); // Desenha um retângulo          
+      // Desenha o quadrado no display (com os valores calculados para x e y)
+      ssd1306_rect(&ssd, y, x, 8, 8, cor, cor); // retângulo no meio do display
+      //ssd1306_rect(&ssd, 28, 60, 8, 8, cor, cor); // retangulo no meio do display
       ssd1306_send_data(&ssd); // Atualiza o display
-  
-  
-      sleep_ms(100);
+
+      // Ativa ou desativa os LEDs PWM com o botão A
+      if (gpio_get(Botao_A)) {
+        pwm_enabled = !pwm_enabled;
+
+        if (!pwm_enabled) {
+          // Desativa os LEDs PWM (sem movimento)
+          pwm_set_gpio_level(LEDR_PIN, 0);
+          pwm_set_gpio_level(LEDG_PIN, 0);
+        }
+      }
+    // Introduz um atraso de 100 milissegundos antes de repetir a leitura
+    sleep_ms(100);  // Pausa o programa por 100ms para evitar leituras e impressões muito rápidas
     }
   }
